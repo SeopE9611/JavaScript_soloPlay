a21a6c010a6c090d00c914021a5bfe015a4888a0
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-07 19:06:38 +0900

    classs 태그의 다양한 접근(index, length,for...of문 등)

--- interactive_JS/01.id태그선택하기/index.js
+++ interactive_JS/01.id태그선택하기/index.js
@@ -2,5 +2,19 @@
 const myTag = document.getElementById('btns')
 console.log(myTag);
 
+console.log('-------------------------------');
+
+
 //class 태그 선택하기
-document.getElementsByClassName('class')
+const myTags = document.getElementsByClassName('color-btn')
+console.log(myTags);
+
+console.log(myTags[1]); //myTags index 접근
+console.log(myTags.length); //myTags length 프로퍼티 활용
+for(let tag of myTags) { //myTags for...of 문으로 활용
+    console.log(tag);
+}
+
+
+
+
