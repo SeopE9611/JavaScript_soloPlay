eb92e3d2050b3ff4536ac5440f10b0a40a57ebb2
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-07 19:23:42 +0900

    하나의 클래스로 접근하려면 index 배열을 통해 접근

--- interactive_JS/01.id태그선택하기/index.js
+++ interactive_JS/01.id태그선택하기/index.js
@@ -6,10 +6,10 @@ console.log('-------------------------------');
 
 
 //class 태그 선택하기
-const myTags = document.getElementsByClassName('color-btn')
+const myTags = document.getElementsByClassName('red')
 console.log(myTags);
 
-console.log(myTags[1]); //myTags index 접근
+console.log(myTags[0]); //myTags index 접근
 console.log(myTags.length); //myTags length 프로퍼티 활용
 for(let tag of myTags) { //myTags for...of 문으로 활용
     console.log(tag);
