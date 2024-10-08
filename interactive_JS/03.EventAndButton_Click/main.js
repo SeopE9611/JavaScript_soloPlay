ae274388a7aba487c8dae7435f141c74d80e3a4d
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-08 15:19:17 +0900

    구체적인 동작들을 코드로 표현한 함수 부분을 '이벤트 핸들러'

--- interactive_JS/03.EventAndButton_Click/main.js
+++ interactive_JS/03.EventAndButton_Click/main.js
@@ -2,6 +2,6 @@
 const btn = document.querySelector('#myBtn')
 
 //이벤트 핸들링 (Event Handling)
-btn.onclick = function() {
+btn.onclick = function() { //이벤트 핸들러 (Event Handler)
     console.log('hello');
 }
