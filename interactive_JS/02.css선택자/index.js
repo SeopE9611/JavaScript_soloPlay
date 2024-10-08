83888e8deac9035ebed4f96d6db10a1fc0ffdca6
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-08 10:35:41 +0900

    CSS 선택자로 태그 선택하기 - querySelector

---
+++ interactive_JS/02.css선택자/index.js
@@ -0,0 +1,7 @@
+// css 선택자로 태그 선택하기
+const myTag = document.querySelector('#myNumber'); // // = getElementById('myNumber')
+console.log(myTag);
+
+const myTag2 = document.getElementById('myNumber'); // = getElementById('myNumber')
+console.log(myTag2);
+
