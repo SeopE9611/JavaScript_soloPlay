14fd04056f1195fcb858c8b4754383c914022279
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-10 20:07:10 +0900

    outerHTML 기본 문법

--- interactive_JS/07.요소노드프로퍼티/index.js
+++ interactive_JS/07.요소노드프로퍼티/index.js
@@ -4,4 +4,7 @@ const myTag = document.querySelector('#list-1')
 //innerHTML
 console.log(myTag.innerHTML);
 // myTag.innerHTML = '<li>Exotic</li>'
-myTag.innerHTML += '<li>Exotic</li>' //할당 연산자 사용
+// myTag.innerHTML += '<li>Exotic</li>' //할당 연산자 사용 
+
+//outerHTML
+console.log(myTag.outerHTML);
