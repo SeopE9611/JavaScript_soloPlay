50d5d98be601374ebddb208962edbe8721ce13af
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-11 19:02:19 +0900

    '마지막' li 태그 만들고 추가

--- interactive_JS/08.요소노드추가하기/index.js
+++ interactive_JS/08.요소노드추가하기/index.js
@@ -19,3 +19,10 @@ const first = document.createElement('li')
 
 // 2. 요소 노드 꾸미기 - textContent, innerHTML....
 first.textContent = '처음'
+
+// 3. 요소 노드 추가하기: NODE.prepend, append, after, before
+tomorrow.prepend(first);
+
+const last = document.createElement('li')
+last.textContent = '마지막'
+tomorrow.append(last)
