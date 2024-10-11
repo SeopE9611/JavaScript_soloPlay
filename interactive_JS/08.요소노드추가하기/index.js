417cc73dd3361e3176891ccedbc2712b6f6c30b8
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-11 18:21:23 +0900

    start

---
+++ interactive_JS/08.요소노드추가하기/index.js
@@ -0,0 +1,15 @@
+// innerHTML, outerHTML의 고찰
+const today = document.querySelector('#today');
+
+today.innerHTML = '<li>처음</li>' + today.innerHTML;
+today.innerHTML = today.innerHTML + '<li>마지막</li>';
+
+today.outerHTML = '<p>이전</p>' + today.outerHTML; 
+
+const newToday = document.querySelector('#today');
+newToday.outerHTML = newToday.outerHTML + '<p>다음</p>';
+
+// 요소 노드 추가하기
+const tomorrow = document.querySelector('#tomorrow');
+
+console.log('--------------------------------------------------');
