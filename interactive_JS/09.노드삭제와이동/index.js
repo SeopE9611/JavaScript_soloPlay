9c1e184d31fbfd521dd1aa90c20a60db57113d95
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-14 19:26:24 +0900

    '내일 할 일'의 두 번째 요소 [뮤지컬 공연 예매]를 '오늘 할 일' 마지막에 넣음

--- interactive_JS/09.노드삭제와이동/index.js
+++ interactive_JS/09.노드삭제와이동/index.js
@@ -3,5 +3,8 @@ const today = document.querySelector('#today');
 const tomorrow = document.querySelector('#tomorrow');
 
 // 노드 삭제하기 : Node.remove()
-tomorrow.remove()
-today.children[2].remove()
+// tomorrow.remove()
+// today.children[2].remove()
+
+// 노드 이동하기: prepend, append, before, after/
+today.append(tomorrow.children[1])
