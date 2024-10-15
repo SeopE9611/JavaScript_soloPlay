4eebb4ceb1c616039da0f2eb8dee8851052a014b
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-15 18:54:10 +0900

    start

---
+++ interactive_JS/10.HTML속성/index.js
@@ -0,0 +1,21 @@
+// HTML 속성 (HTML attribute)
+const tomorrow = document.querySelector('#tomorrow');
+const item = tomorrow.firstElementChild;
+const link = item.firstElementChild;
+
+
+// id 속성
+console.log(tomorrow);
+console.log(tomorrow.id);
+
+// class 속성
+console.log(item);
+console.log(item.className);
+
+// href 속성
+console.log(link);
+console.log(link.href);
+console.log(tomorrow.href);
+
+
+
