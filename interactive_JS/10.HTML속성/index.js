40fa83d7f9346239baf5f1682e272e7d4b568c10
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-15 18:54:58 +0900

    getAttribute를 사용하여 속성에 접근

--- interactive_JS/10.HTML속성/index.js
+++ interactive_JS/10.HTML속성/index.js
@@ -4,18 +4,22 @@ const item = tomorrow.firstElementChild;
 const link = item.firstElementChild;
 
 
-// id 속성
-console.log(tomorrow);
-console.log(tomorrow.id);
+//elem.getAttribute('속성') - 속성에 접근하기
+console.log(tomorrow.getAttribute('href'));
+console.log(item.getAttribute('class'));
 
-// class 속성
-console.log(item);
-console.log(item.className);
+// // id 속성
+// console.log(tomorrow);
+// console.log(tomorrow.id);
 
-// href 속성
-console.log(link);
-console.log(link.href);
-console.log(tomorrow.href);
+// // class 속성
+// console.log(item);
+// console.log(item.className);
+
+// // href 속성
+// console.log(link);
+// console.log(link.href);
+// console.log(tomorrow.href);
 
 
 
