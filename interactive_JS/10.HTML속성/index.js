a88a586c1bd330d8053fd31f381cdaea97c62570
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-15 19:40:19 +0900

    removeAttribute를 사용하여 속성 제거하기

--- interactive_JS/10.HTML속성/index.js
+++ interactive_JS/10.HTML속성/index.js
@@ -10,10 +10,15 @@ console.log(item.getAttribute('class'));
 
 console.log('-----------------------------------------');
 
-
 //elem.setAttribute('속성','값') - 속성 추가(수정)하기
 tomorrow.setAttribute('class', 'list') // 추가
 link.setAttribute('href', 'https://www.naver.com') // 수정
+
+console.log('-----------------------------------------');
+
+//elem.removeAttribute('속성') - 속성 제거하기
+tomorrow.removeAttribute('hReF')
+tomorrow.removeAttribute('ClAsS')
 
 // // id 속성
 // console.log(tomorrow);
