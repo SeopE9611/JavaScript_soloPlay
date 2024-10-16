280d2b8acd22499a8970697f189e4784743974fe
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-16 16:21:53 +0900

    style프로퍼티 + 카멜표기법을 활용하여 글자 스타일을 다룸

--- interactive_JS/11.스타일적용/index.js
+++ interactive_JS/11.스타일적용/index.js
@@ -3,4 +3,7 @@ const today = document.querySelector('#today');
 const tomorrow = document.querySelector('#tomorrow');
 
 
+//style 프로퍼티
+today.children[0].style.textDecoration = 'line-through'
+today.children[0].style.backgroundColor = '#DDDDDD'
 
