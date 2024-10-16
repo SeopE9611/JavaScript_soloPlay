8e4a859452879234ebb45e2f1a731f9f2a4fe777
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-16 16:23:16 +0900

    className 프로퍼티를 활용하여 미리 작성해둔 'done' 스타일로 접근

--- interactive_JS/11.스타일적용/index.js
+++ interactive_JS/11.스타일적용/index.js
@@ -2,6 +2,8 @@
 const today = document.querySelector('#today');
 const tomorrow = document.querySelector('#tomorrow');
 
+//elem.className
+today.children[1].className = 'done'
 
 //style 프로퍼티
 today.children[0].style.textDecoration = 'line-through'
