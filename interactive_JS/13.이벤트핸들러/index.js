7657743c3f3d1c8d886e1b09986fa51e2108de38
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-05 21:14:48 +0900

    이벤트 핸들러안에 여러개 이벤트 핸들러를 넣는 방식

--- interactive_JS/13.이벤트핸들러/index.js
+++ interactive_JS/13.이벤트핸들러/index.js
@@ -1,10 +1,25 @@
 //이벤트 핸들러 등록하기
 let btn = document.querySelector("#myBtn");
 
+/*
 btn.onclick = function () {
   console.log("Hi");
 };
 
 btn.onclick = function () {
   console.log("HiHi");
+};
+*/
+
+function event1() {
+  console.log("Hi");
+}
+
+function event2() {
+  console.log("HiHi");
+}
+
+btn.onclick = function () {
+  event1();
+  event2();
 };
