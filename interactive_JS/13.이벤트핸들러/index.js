7f97d8a30afb1e85a43ad9a425442721ef3a988b
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-06 18:48:47 +0900

    이벤트 핸들러 사용시 주의할 점

--- interactive_JS/13.이벤트핸들러/index.js
+++ interactive_JS/13.이벤트핸들러/index.js
@@ -35,8 +35,17 @@ function event2() {
 }
 
 //elem.addEventListner[event, handler]
-btn.addEventListener("click", event1);
+btn.addEventListener("click", event1());
 btn.addEventListener("click", event2);
 
 //elem.removeEventListner[event, handler]
 btn.removeEventListener("click", event2);
+
+//이벤트를 삭제하려면 반드시 등록할 때 사용했던 핸들러를 그대로 전달해야한다.
+btn.addEventListener("click", function () {
+  console.log("event3");
+});
+
+btn.removeEventListener("click", function () {
+  console.log("event3");
+});
