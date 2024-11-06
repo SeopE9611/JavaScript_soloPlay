72c1f2927e9c7a0e6d0b893c5843f892d1462358
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-06 19:37:17 +0900

    이벤트 핸들러(키보드,마우스 이벤트) 등록

--- interactive_JS/14.이벤트객체/index.js
+++ interactive_JS/14.이벤트객체/index.js
@@ -1,3 +1,11 @@
 // 이벤트 객체 (Event Object)
 const myInput = document.querySelector("#myInput");
 const myBtn = document.querySelector("#myBtn");
+
+myInput.addEventListener("keydown", function () {
+  console.log("키보드 이벤트");
+});
+
+myBtn.addEventListener("click", function () {
+  console.log("마우스 이벤트");
+});
