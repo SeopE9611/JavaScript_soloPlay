adf8385b5306754dc2bea928fa7452a4465f7911
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-07 19:19:52 +0900

    이벤트 버블링 - 이벤트 객체의 target 프로퍼티 활용

--- interactive_JS/15.이벤트버블링/index.js
+++ interactive_JS/15.이벤트버블링/index.js
@@ -4,20 +4,24 @@ const title = document.querySelector("#title");
 const list = document.querySelector("#list");
 const items = document.querySelectorAll(".item");
 
-content.addEventListener("click", function () {
+content.addEventListener("click", function (e) {
   console.log("content Event");
+  console.log(e.target);
 });
 
-title.addEventListener("click", function () {
+title.addEventListener("click", function (e) {
   console.log("title Event");
+  console.log(e.target);
 });
 
-list.addEventListener("click", function () {
+list.addEventListener("click", function (e) {
   console.log("list Event");
+  console.log(e.target);
 });
 
 for (let item of items) {
-  item.addEventListener("click", function () {
+  item.addEventListener("click", function (e) {
     console.log("item Event");
+    console.log(e.target);
   });
 }
