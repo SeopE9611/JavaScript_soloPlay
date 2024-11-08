3a9fed346b1450145c49d051f6a9d345f21a5b49
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-08 21:22:52 +0900

    start

---
+++ interactive_JS/17.브라우저기본동작/index.js
@@ -0,0 +1,5 @@
+// 브라우저의 기본 동작
+const link = document.querySelector("#link");
+const checkbox = document.querySelector("#checkbox");
+const input = document.querySelector("#input");
+const text = document.querySelector("#text");
