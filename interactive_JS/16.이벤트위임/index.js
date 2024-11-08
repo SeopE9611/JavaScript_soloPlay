82681b3abf6369b592bc580ca36d18e15bfd19be
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-08 17:46:59 +0900

    부모 요소인 리스트에 이벤트 핸들러를 등록하여 앞으로 추가되는 아이템을 자식 요소 이벤트로 다룰 수 있다.

--- interactive_JS/16.이벤트위임/index.js
+++ interactive_JS/16.이벤트위임/index.js
@@ -1,11 +1,15 @@
 // 이벤트 위임 (Event Delegation)
 const list = document.querySelector("#list");
 
-for (let item of list.children) {
-  item.addEventListener("click", function (e) {
-    e.target.classList.toggle("done");
-  });
-}
+// for (let item of list.children) {
+//   item.addEventListener("click", function (e) {
+//     e.target.classList.toggle("done");
+//   });
+// }
+
+list.addEventListener("click", function (e) {
+  e.target.classList.toggle("done");
+});
 
 //새로운 아이템 추가
 const li = document.createElement("li");
