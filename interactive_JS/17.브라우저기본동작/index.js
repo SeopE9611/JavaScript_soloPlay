64ba45774b65ee07124f8295bf2dc42b13478373
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-08 21:27:36 +0900

    preventDefault 메소드를 사용하여 링크를 클릭할 시 이동 불가 경고창이 뜨도록 함

--- interactive_JS/17.브라우저기본동작/index.js
+++ interactive_JS/17.브라우저기본동작/index.js
@@ -3,3 +3,9 @@ const link = document.querySelector("#link");
 const checkbox = document.querySelector("#checkbox");
 const input = document.querySelector("#input");
 const text = document.querySelector("#text");
+
+//event.preventDefault
+link.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert("이동 불가");
+});
