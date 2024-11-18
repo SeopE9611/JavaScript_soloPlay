99fb10f1319e77e77ecdfe02e2a141eff0a66f0c
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-18 19:05:01 +0900

    스크롤 이벤트의 scrollY 프로퍼티 값을 담는 조건문을 추가하고 스크롤의 방향을 감지하여 스크롤 효과 적용

--- interactive_JS/24.스크롤이벤트/index.js
+++ interactive_JS/24.스크롤이벤트/index.js
@@ -1,4 +1,7 @@
 // Scroll 이벤트
+
+let lastScrollY = 0;
+
 function printEvent(e) {
   console.log(window.scrollY);
 
@@ -16,6 +19,16 @@ function printEvent(e) {
     nav.classList.remove("shadow"); // nav요소 노드에 shadow 클래스 삭제
     toTop.classList.remove("show"); // toTop요소 노드에 show 클래스 삭제
   }
+
+  if (window.scrollY > lastScrollY) {
+    // 스크롤 방향이 아래쪽일 때
+    nav.classList.add("lift-up");
+  } else {
+    //스크롤 방향이 위쪽일 떄
+    nav.classList.remove("lift-up");
+  }
+
+  lastScrollY = window.scrollY; // 마지막 스크롤 y의 위치를 다시 재할당
 }
 
 window.addEventListener("scroll", printEvent);
