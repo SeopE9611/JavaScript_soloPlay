455c86bcbcd3287bebef132d83fd79f07b85a2e6
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-19 17:30:18 +0900

    불린 Flasy 값과 Truthy 값

--- Modern_JS/01.Boolean/index.js
+++ Modern_JS/01.Boolean/index.js
@@ -4,3 +4,18 @@ if ("Hello") {
   console.log("Bye");
 }
 // 결과 값은 Hello
+
+// Falsy 값
+console.log(Boolean(false));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(0));
+console.log(Boolean(""));
+
+// Truthy 값
+console.log(Boolean(true));
+console.log(Boolean("SeopE"));
+console.log(Boolean(123));
+console.log(Boolean(-123));
+console.log(Boolean({}));
