07338ad20b76863fa5e157ce90bb34dc2edd82ad
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-19 18:42:51 +0900

    OR 연산자는 왼쪽 값이 Truthy하면 왼쪽 값을 리턴하고 왼쪽 값이 Falsy하면 오른쪽 값을 리턴

--- Modern_JS/02.AND,OR/index.js
+++ Modern_JS/02.AND,OR/index.js
@@ -5,3 +5,10 @@ console.log(true && true);
 console.log(true && false);
 console.log(false && true);
 console.log(false && false);
+
+console.log("------------------------------------");
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
