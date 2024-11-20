ea840b31788fe98828996227bf98f68d0b3373c5
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-20 17:53:37 +0900

    null 병합 연산자와 OR 연산자의 비교

--- Modern_JS/03.null연산자/index.js
+++ Modern_JS/03.null연산자/index.js
@@ -5,3 +5,25 @@ const example2 = undefined ?? "love";
 const example3 = "JavaScript" ?? "SeopE";
 
 console.log(example1, example2, example3);
+
+console.log(`------------------------------------`);
+
+const Name1 = null || "SeopE";
+const Name2 = null ?? "SeopE";
+
+console.log(Name1);
+console.log(Name2);
+
+console.log(`------------------------------------`);
+
+const title1 = false || "codeit";
+const title2 = false ?? "codeit";
+
+console.log(title1);
+console.log(title2);
+
+const width1 = 0 || 150;
+const width2 = 0 ?? 150;
+
+console.log(width1);
+console.log(width2);
