181c1d07fcd687593c4f67220c48d7ea27b5acd8
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-20 20:19:57 +0900

    var 키워드는 함수 단위로만 구분이 되기 때문에 조건문이나 반복문 안에서 새로운 변수를 만들게 되더라도 모두 전역 변수로 평가됨

--- Modern_JS/04.변수와스코프/index.js
+++ Modern_JS/04.변수와스코프/index.js
@@ -12,7 +12,32 @@
 // var title = "JavaScript";
 // console.log(title);
 
-let title = "SeopE";
-console.log(title);
-let title = "JavaScript";
-console.log(title);
+// let title = "SeopE";
+// console.log(title);
+// let title = "JavaScript";
+// console.log(title);
+
+var x = 3; // 전역 변수
+
+function myFunc() {
+  var y = 4; // 지역 변수
+  console.log(`x in myFunc: ${x}`);
+  console.log(`y in myFunc: ${y}`);
+}
+
+myFunc();
+console.log(x);
+console.log(y);
+
+var x = 3;
+if (x < 4) {
+  var y = 3;
+}
+
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+console.log("x:", x);
+console.log("y:", y);
+console.log("i:", i);
