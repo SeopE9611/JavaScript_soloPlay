62d0a49c465b69a6b5e4399e7a8063c8a8258fba
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-20 17:46:06 +0900

    null 병합 연산자 - ?? 사용 예시

---
+++ Modern_JS/03.null연산자/index.js
@@ -0,0 +1,7 @@
+// null 연산자 - Nullish coalescing operator
+
+const example1 = null ?? "I";
+const example2 = undefined ?? "love";
+const example3 = "JavaScript" ?? "SeopE";
+
+console.log(example1, example2, example3);
