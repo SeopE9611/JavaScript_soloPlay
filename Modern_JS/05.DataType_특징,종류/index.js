9b7ec602740332b49bd014e93e100056a3d48526
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-21 17:22:38 +0900

    함수 표현식 - 함수 선언을 값처럼 활용하는 방식

--- Modern_JS/05.DataType_특징,종류/index.js
+++ Modern_JS/05.DataType_특징,종류/index.js
@@ -1,6 +1,14 @@
 // 함수를 만드는 방법
 
-function 함수이름(파라미터) {
-  필요한 동작;
-  return 리턴값;
-}
+/*  function 함수이름(파라미터) {
+      필요한 동작;
+      return 리턴값;
+    }
+*/
+
+// 함수 표현식(Function Expression)
+const printStudy = function () {
+  console.log("Modern JS");
+};
+
+printStudy();
