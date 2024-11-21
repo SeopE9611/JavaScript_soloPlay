1d613bdcd825eb5ddd17b31957b464df439b00c9
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-21 21:35:47 +0900

    🚨

--- Modern_JS/06.기명_함수_표현식/index.js
+++ Modern_JS/06.기명_함수_표현식/index.js
@@ -14,8 +14,39 @@ const sayHi = function printHiInConsole() {
 console.log(sayHi.name); // printHiInConsole
 */
 
+/*
 const sayHi = function printHiInConsole() {
   console.log("Hi");
 };
 
 printHiInConsole(); // ReferenceError
+*/
+
+/*
+let countdown = function (n) {
+  console.log(n);
+ 
+  if (n === 0) {
+    console.log("End!");
+  } else {
+    countdown(n - 1);
+  }
+};
+
+countdown(5);
+*/
+
+let countdown = function (n) {
+  console.log(n);
+  if (n === 0) {
+    console.log("End!");
+  } else {
+    countdown(n - 1);
+  }
+};
+
+let myFunction = countdown;
+
+countdown = null;
+
+myFunction(5); // TypeError
