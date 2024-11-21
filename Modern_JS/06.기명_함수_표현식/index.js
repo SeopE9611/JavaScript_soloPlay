46d0eb954f06f2707190b74b48879d5f10dfe375
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-21 21:03:44 +0900

    함수에 이름을 붙여주게 되면, name 속성은 함수 이름을 문자열로 갖게 됨

--- Modern_JS/06.기명_함수_표현식/index.js
+++ Modern_JS/06.기명_함수_표현식/index.js
@@ -1,5 +1,13 @@
+/*
 const sayHi = function () {
   console.log("Hi");
 };
 
 console.log(sayHi.name); // sayHi
+*/
+
+const sayHi = function printHiInConsole() {
+  console.log("Hi");
+};
+
+console.log(sayHi.name); // printHiInConsole
