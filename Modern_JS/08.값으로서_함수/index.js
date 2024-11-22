17d4c84e450b300faef14be28066342c26520be5
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-22 20:07:13 +0900

    함수의 구체적인 데이터 타입은 객체인 것을 확인

---
+++ Modern_JS/08.값으로서_함수/index.js
@@ -0,0 +1,13 @@
+const printJS = function () {
+  console.log("JavaScript");
+};
+
+console.log("-------------------------------");
+console.dir(0); // 0
+console.dir("SeopE"); // SeopE
+console.dir(true); // true
+console.dir(null); // null
+console.dir(undefined); // undefined
+console.dir({}); // Object
+console.dir([]); // Array
+console.dir(printJS); // f printJS{}
