4c7350762787731b2a7e511b4e8ab3c82c0cef49
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-25 21:03:46 +0900

    파라미터에 기본값처럼 넣어 아규먼트를 생략하거나 파라미터의 기본값을 사용하고 싶다면 아규먼트에 undefined를 직접 적용

--- Modern_JS/09.파라미터/index.js
+++ Modern_JS/09.파라미터/index.js
@@ -1,8 +1,24 @@
 // Parameter
+/*
 function greeting(name = "SeopE") {
   console.log(`my name is ${name}...`);
 }
 
-// greeting("JavaScript");
-// greeting("SeopE");
+greeting("JavaScript");
+greeting("SeopE");
 greeting();
+*/
+
+function greeting(name = "SeopE", interest) {
+  console.log(`my name is ${name}...`);
+  console.log(`I like ${interest}`);
+}
+greeting("JavaScript");
+
+console.log("-----------------------------------");
+
+function greeting(name = "SeopE", interest) {
+  console.log(`my name is ${name}...`);
+  console.log(`I like ${interest}`);
+}
+greeting(undefined, "JavaScript");
