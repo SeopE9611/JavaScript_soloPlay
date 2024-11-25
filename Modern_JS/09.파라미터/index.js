e02edf851f3b46fbdcc21765dd0638d622aafe4e
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-25 21:29:15 +0900

    파라미터의 기본값은 이미 정의된 파라미터를 활용할 수 있다

--- Modern_JS/09.파라미터/index.js
+++ Modern_JS/09.파라미터/index.js
@@ -22,3 +22,12 @@ function greeting(name = "SeopE", interest) {
   console.log(`I like ${interest}`);
 }
 greeting(undefined, "JavaScript");
+
+console.log("-----------------------------------");
+
+function defaultTest(x, y = x + 3) {
+  console.log(`x: ${x}`);
+  console.log(`x: ${y}`);
+}
+defaultTest(2);
+// defaultTest(2,3);
