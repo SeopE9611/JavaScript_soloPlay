1495d520fb277f088245b32b7f8443004444efdb
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-26 20:45:12 +0900

    argument 객체는 유사 배열이기 때문에 배열의 메소드를 사용할 수 없다고 했을 때 레스트 파라미터는 배열이기 때문에 배열의 메소드를 자유롭게 사용함

--- Modern_JS/11.Rest_Parameter/index.js
+++ Modern_JS/11.Rest_Parameter/index.js
@@ -15,10 +15,25 @@ printArguments("Ronaldo", "Messi");
 printArguments("Doran", "Oner", "Deft", "Keria");
 */
 
+/*
 function printArguments(...args) {
   for (const arg of args) {
     console.log(arg);
   }
+  console.log(`--------------------------`);
+}
+printArguments("Jung", "Sang", "hwa");
+printArguments("Shin");
+printArguments("Chang", "Seop");
+
+printArguments("Ronaldo", "Messi");
+printArguments("Doran", "Oner", "Deft", "Keria");
+*/
+
+function printArguments(...args) {
+  console.log(args.splice(0, 2)); //Rest Parameter
+  console.log(arguments.splice(0, 2)); //arguments // Error
+
   console.log(`--------------------------`);
 }
 printArguments("Jung", "Sang", "hwa");
