a2e398ac40d8ca09aa62c1e05696bd66f0024e3b
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-26 21:11:50 +0900

    Rest Parameter를 활용함과 동시에 일반 파라미터 작성

--- Modern_JS/11.Rest_Parameter/index.js
+++ Modern_JS/11.Rest_Parameter/index.js
@@ -30,6 +30,7 @@ printArguments("Ronaldo", "Messi");
 printArguments("Doran", "Oner", "Deft", "Keria");
 */
 
+/*
 function printArguments(...args) {
   console.log(args.splice(0, 2)); //Rest Parameter
   console.log(arguments.splice(0, 2)); //arguments // Error
@@ -42,3 +43,15 @@ printArguments("Chang", "Seop");
 
 printArguments("Ronaldo", "Messi");
 printArguments("Doran", "Oner", "Deft", "Keria");
+*/
+
+function Worlds2024(first, second, ...others) {
+  console.log("2024 월즈 최종결과");
+  console.log(`우승: ${first}`);
+  console.log(`준우승: ${second}`);
+  console.log("--------------------");
+  for (const arg of others) {
+    console.log(`우승맴버: ${arg}`);
+  }
+}
+Worlds2024("T1", "BLG", "Zeus", "Oner", "Faker", "Gumayusi", "Keria");
