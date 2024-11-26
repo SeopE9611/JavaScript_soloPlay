c79ed451ae3f685b05d5ad0f6a437cff57cb6b28
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-26 16:58:13 +0900

    아무런 값도 전달 받지 못한 파라미터가 undefined 값을 갖고 동작

--- Modern_JS/10.Arguments/index.js
+++ Modern_JS/10.Arguments/index.js
@@ -4,5 +4,8 @@ function printArguments(a, b, c) {
   console.log(a);
   console.log(b);
   console.log(c);
+  console.log(`--------------------------`);
 }
 printArguments("Jung", "Sang", "hwa");
+printArguments("Shin");
+printArguments("Chang", "Seop");
