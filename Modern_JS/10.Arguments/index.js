a835900ebbcdbad249126b5c6e1d588f038bf3e8
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-26 17:42:20 +0900

    arguments 객체는 배열과 비슷해 보이지만 모양만 그럴 뿐 배열의 메소드는 사용할 수 없는 유사 배열이지만 length나 인덱싱, for...of문을 사용하여 반복작업도 할 수있다

--- Modern_JS/10.Arguments/index.js
+++ Modern_JS/10.Arguments/index.js
@@ -1,9 +1,55 @@
 // Arguments
 
+/*
 function printArguments(a, b, c) {
   console.log(a);
   console.log(b);
   console.log(c);
+  console.log(arguments);
+  console.log(`--------------------------`);
+}
+printArguments("Jung", "Sang", "hwa");
+printArguments("Shin");
+printArguments("Chang", "Seop");
+
+printArguments("Ronaldo", "Messi");
+printArguments("Doran", "Oner", "Deft", "Keria");
+*/
+/*
+function printArguments(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(arguments.length);
+  console.log(`--------------------------`);
+}
+printArguments("Jung", "Sang", "hwa");
+printArguments("Shin");
+printArguments("Chang", "Seop");
+
+printArguments("Ronaldo", "Messi");
+printArguments("Doran", "Oner", "Deft", "Keria");
+*/
+
+/*
+function printArguments(a, b, c) {
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+  console.log(arguments.length);
+  console.log(`--------------------------`);
+}
+printArguments("Jung", "Sang", "hwa");
+printArguments("Shin");
+printArguments("Chang", "Seop");
+
+printArguments("Ronaldo", "Messi");
+printArguments("Doran", "Oner", "Deft", "Keria");
+*/
+
+function printArguments(a, b, c) {
+  for (const arg of arguments) {
+    console.log(arg);
+  }
   console.log(`--------------------------`);
 }
 printArguments("Jung", "Sang", "hwa");
