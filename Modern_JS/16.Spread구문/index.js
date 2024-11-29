be9f0ab0f241e7d3cae0e794157ab2c571095f75
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-29 20:44:00 +0900

    Spread 구문을 활용하여 메소드를 복사

--- Modern_JS/16.Spread구문/index.js
+++ Modern_JS/16.Spread구문/index.js
@@ -3,9 +3,18 @@
 const number = [1, 2, 3];
 console.log(...number);
 */
-
+/*
 const webPublishing = ["HTML", "CSS"];
 const interactiveweb = webPublishing.slice();
+
+interactiveweb.push("JavaScript");
+
+console.log(webPublishing);
+console.log(interactiveweb);
+*/
+
+const webPublishing = ["HTML", "CSS"];
+const interactiveweb = [...webPublishing]; // Spread
 
 interactiveweb.push("JavaScript");
 
