bc926ef8a0f8973e7332926127b72e439a561d5e
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-29 19:44:50 +0900

    삼항 연산자는 변수를 선언하거나 조건에 따라 반복문 수행 X

--- Modern_JS/15.조건연산자/index.js
+++ Modern_JS/15.조건연산자/index.js
@@ -17,12 +17,32 @@ switch (값) {
 
 // 조건 ? truthy 할 때 표현식 : falsy 할 때 표현식
 // 조건 연산자 = 삼항 연산자(Ternary operator)
+
+/*
 const CUT_OFF = 80;
 function passChecker(score) {
   // return score > CUT_OFF ? "합격" : "불합격";
   if (score > CUT_OFF) {
     return "합격";
   } else {
+    return "불합격";
+  }
+}
+console.log(passChecker(70));
+*/
+
+// 삼항 연산자는 변수를 선언하거나 조건에 따라 반복문 수행 X
+const CUT_OFF = 80;
+function passChecker(score) {
+  // return score > CUT_OFF ? "합격" : "불합격";
+  if (score > CUT_OFF) {
+    const msg = "합격"; // 변수 선언하여 그 변수값을 리턴
+    return msg;
+  } else {
+    for (let i = 0; i < 3; i++) {
+      // 반복문
+      console.log(i);
+    }
     return "불합격";
   }
 }
