36e59758d44017fcf09177ac715b5039b0dad0bb
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-29 18:48:49 +0900

    삼항 연산자 활용

--- Modern_JS/15.조건연산자/index.js
+++ Modern_JS/15.조건연산자/index.js
@@ -1,4 +1,5 @@
 // 조건 연산자 (Conditional operator)
+/*
 if (조건) {
   // 조건이 true 일 때 동작
 } else {
@@ -12,3 +13,12 @@ switch (값) {
   default:
   // 값과 일치하는 case가 없을 때 동작
 }
+*/
+
+// 조건 ? truthy 할 때 표현식 : falsy 할 때 표현식
+// 조건 연산자 = 삼항 연산자(Ternary operator)
+const CUT_OFF = 80;
+function passChecker(score) {
+  return score > CUT_OFF ? "합격" : "불합격";
+}
+console.log(passChecker(70));
