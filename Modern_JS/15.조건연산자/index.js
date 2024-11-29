d52f41567e3d3298e42821dd2cd0009cbe4af878
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-29 18:19:07 +0900

    조건을 다룰 때의 if문과 switch문

---
+++ Modern_JS/15.조건연산자/index.js
@@ -0,0 +1,14 @@
+// 조건 연산자 (Conditional operator)
+if (조건) {
+  // 조건이 true 일 때 동작
+} else {
+  // 조건이 false일 때 동작
+}
+
+switch (값) {
+  case A:
+    // 값이 A와 일치할 때 동작
+    break;
+  default:
+  // 값과 일치하는 case가 없을 때 동작
+}
