54da5551f1488368f61873fdb24225ec850f54a7
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-11-29 20:39:41 +0900

    객체 타입의 값들은 변수에 값이 직접 할당되는 것이 아니라 주소 값이 참조된다(slice 메소드를 활용해야함)

--- Modern_JS/16.Spread구문/index.js
+++ Modern_JS/16.Spread구문/index.js
@@ -1,3 +1,13 @@
 // Spread 구문 (Spread Syntax)
+/*
 const number = [1, 2, 3];
 console.log(...number);
+*/
+
+const webPublishing = ["HTML", "CSS"];
+const interactiveweb = webPublishing;
+
+interactiveweb.push("JavaScript");
+
+console.log(webPublishing);
+console.log(interactiveweb);
