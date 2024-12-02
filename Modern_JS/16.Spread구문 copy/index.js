c65f876ddf657b0663892514a616c1d74650085f
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-02 20:22:31 +0900

    변수에 할당된 값을 활용해서 프로퍼티를 만들 떄 활용할 변수의 이름과 프로퍼티 네임이 같다면 하나만 작성하는 표현이 가능

---
+++ Modern_JS/16.Spread구문 copy/index.js
@@ -0,0 +1,14 @@
+const myName = "SeopE";
+const birth = "0101";
+const job = "백수";
+
+const user = {
+  // myName: myName,
+  // birth: birth,
+  // job: job,
+  myName,
+  birth,
+  job,
+};
+
+console.log(user);
