0e843eec509d1f603e4b5105a24a4f2991dfbf1e
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-02 16:31:44 +0900

    함수를 호출할 때 아규먼트 사

--- Modern_JS/16.Spread구문/index.js
+++ Modern_JS/16.Spread구문/index.js
@@ -32,7 +32,7 @@ const interactiveweb = [...webPublishing, "JavaScript"]; // Spread
 console.log(webPublishing);
 console.log(interactiveweb);
 */
-
+/*
 const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 const arr3 = [...arr1, ...arr2];
@@ -40,3 +40,13 @@ console.log(arr3);
 
 const arr4 = arr1.concat(arr2);
 console.log(arr4);
+*/
+
+const introduce = (myName, birth, job) => {
+  console.log("안녕하세요");
+  console.log(`제 이름은 ${myName} 입니다.`);
+  console.log(`제 생일은 ${birth} 입니다.`);
+  console.log(`직업은 ${job} 입니다.`);
+};
+const myArr = ["섭이", "0101", "백수"];
+introduce(...myArr);
