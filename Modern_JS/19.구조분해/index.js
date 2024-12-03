6a83cb1f4a78af810063c99f97bc8933fa3b84bd
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-03 21:21:44 +0900

    구조 분해의 기본 문법

--- Modern_JS/19.구조분해/index.js
+++ Modern_JS/19.구조분해/index.js
@@ -1,10 +1,12 @@
 // 구조 분해 (Destructuring)
 const rank = ["행섭", "죡팡", "힝카인", "찌듕"];
 
-const macbook = rank[0];
-const ipad = rank[1];
-const airpods = rank[2];
-const coupon = rank[3];
+// const macbook = rank[0];
+// const ipad = rank[1];
+// const airpods = rank[2];
+// const coupon = rank[3];
+
+const [macbook, ipad, airpods, coupon] = rank;
 
 console.log(macbook);
 console.log(ipad);
