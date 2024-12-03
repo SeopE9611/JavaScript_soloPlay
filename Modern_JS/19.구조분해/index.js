775ee89f6e2c26b30f1d404729e8306afa488280
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-03 20:46:22 +0900

    구조 분해 문법 - 객체 Start

---
+++ Modern_JS/19.구조분해/index.js
@@ -0,0 +1,12 @@
+// 구조 분해 (Destructuring)
+const rank = ["행섭", "죡팡", "힝카인", "찌듕"];
+
+const macbook = rank[0];
+const ipad = rank[1];
+const airpods = rank[2];
+const coupon = rank[3];
+
+console.log(macbook);
+console.log(ipad);
+console.log(airpods);
+console.log(coupon);
