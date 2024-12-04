a7754ca285a34f0983ca7884703a5f78d969c58a
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-04 20:44:09 +0900

    하이픈(-)이 있어서 변수로는 사용할 수 없는 이름도 프로퍼티 네임으로는 사용할 수 있다.

--- Modern_JS/20.구조분해(객체)/index.js
+++ Modern_JS/20.구조분해(객체)/index.js
@@ -4,6 +4,7 @@ const macbook = {
   price: 1000000,
   memory: "16GB",
   storage: "1TB",
+  "serial-num": "ABCDEFG123",
 };
 
 // const title = macbook.title;
@@ -12,7 +13,8 @@ const macbook = {
 // const { title, price } = macbook; // 구조 분해
 // const { title, color } = macbook; // // 존재하지 않는 프로퍼티
 // const { title, color = "실버 색상" } = macbook; // 기본값 지정
-const { title: product, ...rest } = macbook; // // product 새로운 변수명 선언
+// const { title: product, ...rest } = macbook; // // product 새로운 변수명 선언
+const { title: product, "serial-num": serialNumber } = macbook;
 
 console.log(product);
-console.log(rest);
+console.log(serialNumber);
