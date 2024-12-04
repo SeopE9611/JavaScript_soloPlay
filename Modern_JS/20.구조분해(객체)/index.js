f6b8d67bf331134b10aff116b8f49776149986eb
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-04 20:03:48 +0900

    객체의 구조분해에서도 프로퍼티 네임 기본값을 지정할 수있다.

--- Modern_JS/20.구조분해(객체)/index.js
+++ Modern_JS/20.구조분해(객체)/index.js
@@ -10,7 +10,7 @@ const macbook = {
 // const price = macbook.price;
 
 // const { title, price } = macbook; // 구조 분해
-const { title, color } = macbook; // // 존재하지 않는 프로퍼티
-
+// const { title, color } = macbook; // // 존재하지 않는 프로퍼티
+const { title, color = "실버 색상" } = macbook; // 기본값 지정
 console.log(title);
-console.log(color); // // 존재하지 않는 프로퍼티
+console.log(color);
