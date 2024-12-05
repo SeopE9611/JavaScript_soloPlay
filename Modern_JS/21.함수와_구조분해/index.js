0b0065524b0970d74ae42b999582e5b23a856688
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-05 17:44:46 +0900

    파라미터 부분에서 구조분해 문법을 활용하고 여러개의 아규먼트를 전달받는 함수가아닌 배열 하나(rank)를 아규먼트로 전달받는 함수로 구현

--- Modern_JS/21.함수와_구조분해/index.js
+++ Modern_JS/21.함수와_구조분해/index.js
@@ -1,4 +1,5 @@
 // 구조 분해 (Destructuring)
+/*
 function getArray() {
   return ["컴퓨터", "냉장고", "세탁기"];
 }
@@ -7,3 +8,23 @@ const [el1, el2, el3] = getArray();
 console.log(el1);
 console.log(el2);
 console.log(el3);
+*/
+
+// function printWinners(...arg) {
+function printWinners([macbook, ipad, airpods, ...coupon]) {
+  // const [macbook, ipad, airpods, ...coupon] = arg;
+
+  console.log(`맥북 당첨자: ${macbook}`);
+  console.log(`아이패드 당첨자: ${ipad}`);
+  console.log(`에어팟 당첨자: ${airpods}`);
+  console.log(`무신사 쿠폰 당첨자는 다음과 같습니다.`);
+
+  for (let user of coupon) {
+    console.log(`${user}`);
+  }
+  console.log(`쿠폰 수령 인원: ${coupon.length} 명`);
+}
+
+// printWinners("행섭", "양천", "힝카인", "스기따라", "찌듕", "넌적혈구");
+const rank = ["행섭", "양천", "힝카인", "스기따라", "찌듕", "넌적혈구"];
+printWinners(rank);
