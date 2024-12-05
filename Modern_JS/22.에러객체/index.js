ccd6084f6f8f2123563c287c0fdc7a4139ec18a8
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-05 21:21:40 +0900

    에러 객체중 대표적인 에러 3가지

---
+++ Modern_JS/22.에러객체/index.js
@@ -0,0 +1,31 @@
+// 에러 객체 (name, message)
+//1. ReferenceError
+console.log("Start");
+
+const title = "섭이";
+console.log(title);
+// console.log(myName);
+
+console.log("End");
+
+console.log(`------------------------`);
+
+//2.TypeError
+console.log("Start");
+
+const title2 = "섭이";
+// console.log(title());
+// console.log(myName);
+
+console.log("End");
+
+console.log(`------------------------`);
+
+//3.SyntaxError
+console.log("Start");
+
+const ti-tle3 = "섭이";
+console.log(title);
+console.log(myName);
+
+console.log("End");
