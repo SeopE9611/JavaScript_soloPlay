973a296434496698ed83733cb1c49645cbaf9a0c
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-06 20:28:36 +0900

    try catch문의 기본적인 동작 원리

---
+++ Modern_JS/23.try...catch문/02/index.js
@@ -0,0 +1,22 @@
+// try catch문
+/*
+try {
+  // 코드
+} catch (error) {
+  // 에러가 발생했을 때 동작할 코드
+}
+*/
+
+try {
+  console.log("Start");
+
+  const title = "섭이";
+  console.log(title);
+
+  // title = "섭이";
+
+  const language = "JavaScript";
+  console.log(language);
+} catch (error) {
+  console.log("End");
+}
