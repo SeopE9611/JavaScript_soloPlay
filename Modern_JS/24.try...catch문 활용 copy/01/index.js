859bc338c375a54bae27b78ec0daf2523ae6a95b
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-09 21:26:04 +0900

    forEach 메소드는 메소드를 호출할 때 아규먼트로 콜백 함수를 작성해 주면 콜백 함수의 첫 번째 파라미터로 배열의 요소를 하나씩 전달하면서 매번 함수를 실행하는 원리로 동작

--- Modern_JS/24.try...catch문 활용 copy/01/index.js
+++ Modern_JS/24.try...catch문 활용 copy/01/index.js
@@ -4,3 +4,9 @@ const members = ["행섭", "스기따라", "양천", "힝카인"];
 for (let member of members) {
   console.log(`${member}님이 접속하였습니다.`);
 }
+
+console.log(`-------------------------------------`);
+
+members.forEach(function (member) {
+  console.log(`$(member) 님이 접속하였습니다. `);
+});
