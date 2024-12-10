c179f6095f7362ee166ed51e926afa8376dcda5b
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-10 16:49:20 +0900

    메소드를 호출한 결과를 변수에 담아서 콜백 함수의 리턴값들로 구성된 배열을 출력.

--- Modern_JS/25.forEach,map/03/index.js
+++ Modern_JS/25.forEach,map/03/index.js
@@ -3,6 +3,8 @@
 const firstName = ["형섭", "명섭", "민섭", "지섭"];
 const lastName = ["윤", "융", "용", "영"];
 
-firstName.map((firstName, i) => {
+const fullNames = firstName.map((firstName, i) => {
   return lastName[i] + firstName;
 });
+
+console.log(fullNames);
