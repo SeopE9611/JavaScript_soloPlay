9d6b77ca37fae83c143aa92b26e9cfdedef79c42
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-10 17:55:39 +0900

    forEach는 리턴값이 없기 때문에 변수에 담았을땐 undefined 값을 가진다.

--- Modern_JS/25.forEach,map/03/index.js
+++ Modern_JS/25.forEach,map/03/index.js
@@ -13,6 +13,7 @@ console.log(fullNames);
 const firstName = ["형섭", "명섭", "민섭", "지섭"];
 const lastName = ["윤", "융", "용", "영"];
 
-const fullNames = firstName.map((firstName, i) => lastName[i] + firstName);
+// const fullNames = firstName.map((firstName, i) => lastName[i] + firstName);
+const fullNames = firstName.forEach((firstName, i) => lastName[i] + firstName);
 
 console.log(fullNames);
