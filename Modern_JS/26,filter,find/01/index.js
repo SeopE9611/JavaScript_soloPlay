c274bb3ed0040f4e803521b49c265a3f783135f8
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-10 21:39:06 +0900

    filter 메소드로 메소드를 호출한 배열을 반복하면서 콜백함수가 리턴하는 조건식이 true가 되는 요소만 모아서 새로운 배열을 리턴

---
+++ Modern_JS/26,filter,find/01/index.js
@@ -0,0 +1,15 @@
+// filter와 find
+const BossGroup = [
+  { name: "행섭", job: "비숍", type: "마법사" },
+  { name: "양천", job: "캐논마스터", type: "해적" },
+  { name: "스기따라", job: "배틀메이지", type: "마법사" },
+  { name: "힝카인", job: "카인", type: "궁수" },
+  { name: "귀여운적혈구", job: "다크나이트", type: "전사" },
+  { name: "저섭", job: "바이퍼", type: "해적" },
+  { name: "찌듕", job: "아크메이지:불독", type: "마법사" },
+  { name: "넌적혈구", job: "아델", type: "전사" },
+  { name: "메르미르시츄", job: "메르세데스", type: "궁수" },
+];
+
+const types = BossGroup.filter((el) => el.type === "마법사");
+console.log(types);
