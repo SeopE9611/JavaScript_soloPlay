601d453a4ca6480ad67dd717b845b78c6461daa5
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-10 16:45:56 +0900

    콜백 함수 내에서 리턴문을 작성해주면 각각의 리턴값으로 구성된 새로운 배열이 메소드의 결과로 리턴

--- Modern_JS/25.forEach,map/03/index.js
+++ Modern_JS/25.forEach,map/03/index.js
@@ -3,7 +3,6 @@
 const firstName = ["형섭", "명섭", "민섭", "지섭"];
 const lastName = ["윤", "융", "용", "영"];
 
-firstName.map((firstName, i, arr) => {
-  console.log(`${lastName[i]}${firstName}님 환영합니다.`);
-  console.log(arr);
+firstName.map((firstName, i) => {
+  return lastName[i] + firstName;
 });
