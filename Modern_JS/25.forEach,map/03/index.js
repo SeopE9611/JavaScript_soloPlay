3eab7f486282db1a539b15d9de1ddd8799174f4e
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-10 17:55:03 +0900

    화살표 함수의 활용

--- Modern_JS/25.forEach,map/03/index.js
+++ Modern_JS/25.forEach,map/03/index.js
@@ -1,10 +1,18 @@
 // forEach와 map
 
+/*
 const firstName = ["형섭", "명섭", "민섭", "지섭"];
 const lastName = ["윤", "융", "용", "영"];
 
 const fullNames = firstName.map((firstName, i) => {
   return lastName[i] + firstName;
 });
+
+console.log(fullNames);
+*/
+const firstName = ["형섭", "명섭", "민섭", "지섭"];
+const lastName = ["윤", "융", "용", "영"];
+
+const fullNames = firstName.map((firstName, i) => lastName[i] + firstName);
 
 console.log(fullNames);
