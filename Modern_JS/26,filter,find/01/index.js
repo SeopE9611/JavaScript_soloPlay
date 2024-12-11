a2281ea8a99c69a3c51b2ad5a069fdb28fb25bc4
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-11 16:21:48 +0900

    filter로 고유한 값을 찾으러면 spread 함수로 배열을 펼쳐야한다.

--- Modern_JS/26,filter,find/01/index.js
+++ Modern_JS/26,filter,find/01/index.js
@@ -13,3 +13,6 @@ const BossGroup = [
 
 const types = BossGroup.filter((el) => el.type === "마법사");
 console.log(types);
+
+const jobs = BossGroup.filter((el) => el.job === "바이퍼")
+console.log(...jobs);
