3283e5c96d8d4cd14c95f9f9fbccc7ceaccf125c
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-12 20:34:14 +0900

    reverse의 동작 원리

---
+++ Modern_JS/29.sort,reverse/03/index.js
@@ -0,0 +1,8 @@
+const letters = ['a', 'c', 'b'];
+const numbers = [421, 721, 353];
+
+letters.reverse();
+numbers.reverse();
+
+console.log(letters); // (3) ["b", "c", "a"]
+console.log(numbers); // (3) [353, 721, 421]
