82940d28e396891c8d6abeaacaf0be50ea5d109f
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-13 20:14:37 +0900

    배열 내에서 중복을 제거한 값들의 묶음을 만들 때 Set을 사용하기도한다.

--- Modern_JS/30.map,set/03/index.js
+++ Modern_JS/30.map,set/03/index.js
@@ -11,3 +11,10 @@ members.add('힝카인'); // Set(4) {"행섭,양천,스기따라,힝카인"}
 members.add('힝카인'); // Set(4) {"행섭,양천,스기따라,힝카인"}
 members.add('양천'); // Set(4) {"행섭,양천,스기따라,힝카인"}
 members.add('힝카인'); // Set(4) {"행섭,양천,스기따라,힝카인"}
+
+console.log(`-----------------------`);
+
+const numbers = [1, 3, 4, 3, 3, 3, 2, 1, 1, 1, 5, 5, 3, 2, 1, 4];
+const uniqNumbers = new Set(numbers);
+
+console.log(uniqNumbers); // set(5) {1, 3, 4, 2, 5}
