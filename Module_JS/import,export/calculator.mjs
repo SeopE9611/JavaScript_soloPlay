7d4510d766b2d177ddf8b55097909cf1be7e48d8
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-20 20:37:44 +0900

    import와 export

---
+++ Module_JS/import,export/calculator.mjs
@@ -0,0 +1,7 @@
+//calculator.mjs
+
+export const PI = 3.14;
+
+export function add(a, b) {
+  return a + b;
+}
---
+++ Module_JS/import,export/main.mjs
@@ -0,0 +1,5 @@
+// main.mjs
+
+import { PI, add } from './calculator.mjs';
+console.log(PI);
+console.log(add(1, 2));
