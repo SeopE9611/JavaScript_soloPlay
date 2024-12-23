8cb8a513c3cde2d49e151b16c69d0b83fb9d1126
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-23 20:17:08 +0900

    모듈 하나, 즉 파일 하나마다 default export를 최대 한 번씩만 쓸 수 있고 named export, import와 default export, import의 특징을 확인

---
+++ Module_JS/02.default export/02/calculator.mjs
@@ -0,0 +1,9 @@
+//calculator.mjs
+
+const PI = 3.14;
+
+export default PI;
+
+export function add(a, b) {
+  return a + b;
+}
---
+++ Module_JS/02.default export/02/main.mjs
@@ -0,0 +1,6 @@
+// main.mjs
+
+import  PI, { add } from './calculator.mjs';
+
+console.log(PI);
+console.log(add(1, 2));
