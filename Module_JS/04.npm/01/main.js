5481751a8343ca33281ae92fa6fd82548d4a8f72
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-12-25 19:52:34 +0900

    js 확장자로 변경 후 calculator파일을 export

--- Module_JS/04.npm/01/main.js
+++ Module_JS/04.npm/01/main.js
@@ -1,7 +1,9 @@
 // main.mjs
 
-import { format }  from 'date-fns'
+import { format }  from 'date-fns';
+import { add } from './calculator.js';
 
 const dateString = format(new Date(2024,12,24), 'MM/dd/yyyy')
 
 console.log(dateString);
+console.log(add(1,2));
