92e99d7fe09110db224674e1d0b6337e7f18c60f
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-01-01 18:41:14 +0900

    세 개의 비동기 함수를 사용

---
+++ Async_JS/Callback Hell/01/main.js
@@ -0,0 +1,41 @@
+// main.js
+import {getEmployees, json, groupEmployees} from './function.js'
+
+// getEmployees((response)=> console.log(response));
+
+// json('[{ "id": 1, "name": "Jason", "email": "jason@codeitmall.kr", "department": "engineering" }, { "id": 2, "name": "Alice", "email": "alice@codeitmall.kr", "department": "engineering" }, { "id": 3, "name": "Brian", "email": "brian@codeitmall.kr", "department": "marketing" }, { "id": 4, "name": "Erica", "email": "erica@codeitmall.kr", "department": "marketing" }, { "id": 5, "name": "Wilson", "email": "wilson@codeitmall.kr", "department": "sales" }]', 
+//   (data) => console.log(data));
+
+groupEmployees([
+  {
+    id: 1,
+    name: 'Jason',
+    email: 'jason@codeitmall.kr',
+    department: 'engineering'
+  },
+  {
+    id: 2,
+    name: 'Alice',
+    email: 'alice@codeitmall.kr',
+    department: 'engineering'
+  },
+  {
+    id: 3,
+    name: 'Brian',
+    email: 'brian@codeitmall.kr',
+    department: 'marketing'
+  },
+  {
+    id: 4,
+    name: 'Erica',
+    email: 'erica@codeitmall.kr',
+    department: 'marketing'
+  },
+  {
+    id: 5,
+    name: 'Wilson',
+    email: 'wilson@codeitmall.kr',
+    department: 'sales'
+  }
+], (results) => console.log(results));
+
