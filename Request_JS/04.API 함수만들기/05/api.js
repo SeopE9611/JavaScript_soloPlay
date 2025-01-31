69c43e58c7cc2737fa917ec1792bd42d986e269f
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-01-31 20:36:09 +0900

    createColorSurvey 함수 생성하고 surveyData를 파라미터로 받음

---
+++ Request_JS/04.API 함수만들기/05/api.js
@@ -0,0 +1,40 @@
+// api.js
+
+// GET /api/color-surveys를 하는 함수 생성
+
+export async function getColorSurveys(params ={}){
+  const url = new URL('https://learn.codeit.kr/api/color-surveys');
+  Object.keys(params).forEach((key) => 
+    url.searchParams.append(key, params[key])
+    );
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return data; 
+}
+
+// ----------------------------------------------------------------------
+
+// GET /api/color-surveys:id
+export async function getColorSurvey(id){ // getColorSurvey 함수 생성하고 id를 파라미터로 받음
+  const res = await fetch(`https://learn.codeit.kr/api/color-surveys/${id}`); // 백틱을 사용하여 id를 URL에 추가
+  const data = await res.json();
+  return data;
+}
+
+// ----------------------------------------------------------------------
+
+// POST /api/color-surveys
+export async function createColorSurvey(surveyData){ // createColorSurvey 함수 생성하고 surveyData를 파라미터로 받음
+  const res = await fetch('https://learn.codeit.kr/api/color-surveys',{ 
+    method: 'POST', 
+    body: JSON.stringify(surveyData), 
+    headers: {
+      'Content-Type': 'application/json', 
+    }
+  });
+
+  const data = await res.json(); 
+  return data;
+}
