1a95c4766d5427d5f4fba7c3cb764101794156ca
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-02-05 19:20:43 +0900

    문제풀이

---
+++ Request_JS/00.문제풀이/06.axios 오류처리/main.js
@@ -0,0 +1,19 @@
+import { createAvatar } from './api.js';
+
+try {
+  const data = await createAvatar({
+    hairType: 'long5',
+    hairColor: 'black',
+    skin: 'tone100',
+    clothes: 'hoodie',
+    accessories: 'none',
+  });
+  console.log(data);
+} catch (e) {
+  if (e.response) {
+    console.log(e.response.status);
+    console.log(e.response.data);
+  }else {
+    console.log(e.message);
+  }
+}
